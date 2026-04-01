9bca26eca5362c3e2caa4856b4e8b4c0575823fb
X <X@x.x>
2026-04-01 13:27:56 +0200

    X

--- chat aplikace/index.php
+++ chat aplikace/index.php
@@ -7,19 +7,24 @@
 </head>
 <body>
     <form action="" method="post">
-        <input type="text" name="name" id="name">
-        <input type="text" name="msg" id="msg" >
+        Username: <input type="text" name="name" id="name"><br>
+        Message: <input type="text" name="msg" id="msg" ><br>
         <button type="submit" name="submit">Send</button>
     </form>
     <hr>
     <?php 
+    /* 
+    */
     if(isset($_POST["submit"])){
         $name = $_POST["name"];
         $msg = $_POST["msg"];
-
-        echo "$name: <br> $msg <br><br>";
+        
+        echo "$name: ". date("d-m-Y H:i"). "<br> $msg <br><br>";
     }    
     ?>
-    <script src="script.js"></script>
+    <script src="script.js">
+        
+        event.preventDefault();
+    </script>
 </body>
 </html>
